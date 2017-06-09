f3c832598652e01dcf92aa3b43dbca9ebd27beb0
tjgodwin1 <tgodwin@atl.yearup.org>
2017-06-09 16:42:10 -0400

    shopping cart js
    this is the javascript portion of the project...

---
+++ shopping-cart.js
@@ -0,0 +1,99 @@
+
+ function item(name , price , quantity)
+ 	{
+ 		this.name = name;
+ 		this.price = price;
+ 		this.quantity = quantity;
+ 		}
+ 
+var braveTick = new item("Braves Ticket","Price","Quantity");
+var hawkTick = new item ("Atlanta Hawks Ticket","Price","Quantity");
+var totalAndTax = (hawkTickPrice*tax + braveTickPrice*tax + shipping)
+    
+
+//code for calculating math
+var braveTickPrice = 25.00;
+var hawkTickPrice = 35.00;
+var tax = 0.075;
+var shipping = 5.00;
+
+  //*****Greeting******
+
+$(document).ready(function(){
+    $("document").click(function(){
+        alert("Hello and welcome!! ^_^");
+    });
+});
+//if that syntax is correct it should alert greeting as soon as doc is ready
+
+
+
+  //********Shopping Cart********** I know it's supposed to be an array of objs but need help
+  var shoppingCart = []; 
+
+
+
+
+//total over 100
+if(totalAndTax => 100){
+	alert("You are eligible to complete your transaction with a credit line!")
+}
+
+else{
+	alert("Please note that all transactions that take place under $100 require cash payment only!")
+}
+
+
+
+//this should loop thru the items in cart but I do not know how to make an array of objects
+for (i = 0; i < shoppingCart.length; i++){
+}
+
+
+
+
+
+//code for interactive page... when quantity is changed, so should adjusted prices
+//braves
+$(document).ready(function(){
+    $("#braveQuan").change(function(){
+      var braveQuan = $(this).val();
+      $("#div1").find("#braveAdjustPrice").text(braveTickPrice * braveQuan);
+    });
+});
+
+//hawks
+$(document).ready(function(){
+    $("#hawkQuan").change(function(){
+      var hawkQuan = $(this).val();
+      $("#div2").find("#hawkAdjustPrice").text(hawkTickPrice * hawkQuan)
+    });
+});
+
+//subtotal
+$(document).ready(function(){
+    $("input").val(function(){
+      $("#div3").find("#almostTotal").val(braveAdjustPrice + hawkAdjustPrice);
+    });
+});
+
+//tax*********stuck here***********888
+$(document).ready(function(){
+    $("#hawkQuan").val(function(){
+      $("#div2").find("#hawkAdjustPrice").val(hawkTick * hawkQuan)
+    });
+});
+
+//grand total
+var totalAndTax = 
+$(document).ready(function(){
+    $("input").val(function(){
+      $("#div3").find("#grandTotal").val(braveAdjustPrice + hawkAdjustPrice + tax + shipping);
+    });
+});
+
+
+
+
+
+//**********noticed that the arrows are only in chrome
